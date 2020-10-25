d4cba0830d91ae46b39cacca5ab58e36570b3017
Sumaiyya Burney <33647766+sumaiyyab@users.noreply.github.com>
2020-10-24 20:22:02 -0700

    Add files via upload

---
+++ VitaminKRecipesTracker/app/src/main/java/com/example/vitamin_k_recipes_tracker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.vitamin_k_recipes_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
