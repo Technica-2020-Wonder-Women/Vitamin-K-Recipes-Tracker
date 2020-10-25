f8adb662df3f588846cf0f994ac20113ccfcf13a
Sumaiyya Burney <33647766+sumaiyyab@users.noreply.github.com>
2020-10-24 21:20:01 -0700

    Calculates average of 7 user-inputted values

--- VitaminKRecipesTracker/app/src/main/java/com/example/vitamin_k_recipes_tracker/MainActivity.java
+++ VitaminKRecipesTracker/app/src/main/java/com/example/vitamin_k_recipes_tracker/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.vitamin_k_recipes_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.DatePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +22,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void avg(View view){
+        double sum = 0;
+        String prefix = "editTextNumberDecimal";
+        for (int i = 1; i <= 7; i++) {
+            String idName = prefix.concat(Integer.toString(i));
+            int id = getResources().getIdentifier(idName, "id", getPackageName());
+            EditText et = (EditText) findViewById(id);
+            String strVal = et.getText().toString();
+            Double val = Double.parseDouble(strVal);
+            sum += val;
+        }
+        double avg = sum/7.0;
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(Double.toString(avg));
+    }
+
+
 }
